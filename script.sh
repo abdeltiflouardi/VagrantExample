ec1a74a148b36d3055bdbc068df8f03e94a61a74
LOUARDI Abdeltif <louardi.abdeltif@07zr.com>
2015-02-10 14:00:12 +0000

    Add httpd.conf

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ sudo cp /vagrant/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
 
 sudo yum update -y
 
-sudo yum install httpd wget nano php php-mcrypt* mongodb-org mongodb-org-server mod_ssl -y
+sudo yum install httpd wget nano lynx php php-mcrypt* mongodb-org mongodb-org-server mod_ssl -y
 
 sudo systemctl start httpd
 sudo systemctl start mongod
@@ -17,8 +17,11 @@ sudo chkconfig mongod on
 sudo rm -f /etc/httpd/conf.d/welcome.conf
 sudo rm -f /var/www/error/noindex.html
 
+sudo cp /vagrant/files/httpd.conf /etc/httpd/conf/httpd.conf
 sudo cp /vagrant/files/dev.conf /etc/httpd/conf.d/dev.conf
 sudo cp /vagrant/files/dev-ssl.conf /etc/httpd/conf.d/dev-ssl.conf
 
-sudo bash -c  'echo -e "[DATE]\n//Notre Timezone\ndate.timezone = "GMT"\n\n"  >>  /etc/php.d/timezone.ini'
-sudo bash -c  'echo -e "\nxdebug.max_nesting_level = 250\n"  >>  /etc/php.d/xdebug.ini'
+sudo bash -c 'echo -e "[DATE]\n//Notre Timezone\ndate.timezone = "GMT"\n\n"  >>  /etc/php.d/timezone.ini'
+sudo bash -c 'echo -e "\nxdebug.max_nesting_level = 250\n"  >>  /etc/php.d/xdebug.ini'
+
+sudo bash -c 'echo "192.168.1.200 dev" >> /etc/hosts'
